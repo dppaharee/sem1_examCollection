f1fe444a154019b8eba472b678403780433b9dfc
Dishant Pahari <dppaharee@gmail.com>
2026-04-23 09:21:45 +0545

    variable changed

--- palindrome.c
+++ palindrome.c
@@ -2,14 +2,14 @@
 #include<stdlib.h>
 int main()
 {
-	int arm=0,num,i;
+	int pal=0,num,i;
     printf("enter a number to check:");
 	scanf("%d",&num);
 	for (i=num;i>0;i=i/10)
 	{
-		arm=arm*10+i%10;
+		pal=pal*10+i%10;
 	}
-	if (num==arm)
+	if (num==pal)
 	{
 		printf("number is palindrome");	
 	}
